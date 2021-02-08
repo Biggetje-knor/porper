13883f1572fffa56fe89164890279a83e41c5f5d
Harold de Jong <hrjong@ziggo.nl>
2021-02-08 12:50:24 +0100

    5e commit t/m 43

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def all_blog(request):
+    blogs = Blog.objects.all()
+    return render(request,'blog/all_blog.html',{'blog':blogs})
+
+
+
