9c508062ec9c5576da9ce80f24031185a81d74a1
Harold de Jong <hrjong@ziggo.nl>
2021-02-08 16:10:00 +0100

    7e commit t/m 46

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blog(request):
-    blogs = Blog.objects.all()
-    return render(request,'blog/all_blog.html',{'blog':blogs})
+    blog = Blog.objects.all()
+    return render(request,'blog/all_blog.html',{'blog':blog})
+
+def detail(request, blog_id):
+    blogs = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html',{'blog':blogs})
+
 
 
 
